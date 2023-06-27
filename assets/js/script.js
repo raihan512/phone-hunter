b05c4c1bc4ed05880afd744a2a5f429c7fe5c4c4
raihan512 <raihangazi1024@gmail.com>
2023-06-27 21:31:41 +0600

    Product details Modal added

--- assets/js/script.js
+++ assets/js/script.js
@@ -43,14 +43,15 @@ const displayPhone = phones => {
         noPhone.classList.add('d-none')
         phones.forEach(phone => {
             const newPhone = document.createElement("div");
-            newPhone.classList.add("col-md-3")
+            newPhone.classList.add("col-sm-4", "col-lg-3", "col-xl-2")
     
             newPhone.innerHTML = `
                 <div class="card">
                     <img src="${phone.image}" class="card-img-top py-3 p-5" alt="...">
                     <div class="card-body">
-                        <h5 class="card-title">${phone.phone_name}</h5>
-                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <h5 class="card-title text-truncate">${phone.phone_name}</h5>
+                        <p class="card-text">This is a longer card with supporting text. This content is a little bit longer.</p>
+                        <button type="button" onClick=(getPhoneDetails('${phone.slug}')) class="btn btn-primary btn-sm" data-bs-toggle="modal" data-bs-target="#exampleModal">Show Details</button>
                     </div>
                 </div>
             `
@@ -70,4 +71,39 @@ const loading = isLoading => {
     }
 }
 
+const getPhoneDetails = async id => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`);
+    const details = await res.json();
+    const phoneDetails = details.data;
+    console.log(phoneDetails);
+
+    
+    const phoneName = document.getElementById("phone-name");
+    phoneName.innerHTML = phoneDetails.name;
+
+    const modalBody = document.getElementById("modal-body");
+    modalBody.innerHTML = `
+    <div>
+        <img src="${phoneDetails.image}" class="p-3" />
+    </div>
+    <div>
+        <p class="fw-bold my-0 py-0">${phoneDetails.name}</p>
+        <p class="my-0 py-0">Brand: ${phoneDetails.brand}</p>
+        <p class="my-0 py-0">${phoneDetails.releaseDate}</p>
+        
+        <p class="border-bottom mt-1 mb-1 fw-bold my-0 py-0 mb-1">Main Features</p>
+
+        ${Object.keys(phoneDetails.mainFeatures).map( (item) => `<p class="my-0 py-0"><small><span class="fw-bold text-uppercase">${item}:</span> ${phoneDetails.mainFeatures[item]}</small></p>`).join('')}
+
+
+
+        <p class="border-bottom mt-1 mb-1 fw-bold my-0 py-0 mb-1">Others</p>
+        ${Object.keys(phoneDetails.others).map( (item) => `<p class="my-0 py-0"><small><span class="fw-bold text-uppercase">${item}:</span> ${phoneDetails.others[item]}</small></p>`).join('')}
+
+
+    </div>
+    `
+
+
+}
 loadPhones('a');
